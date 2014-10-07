51a9161da6f4bd3a539296e425c2437fb279ee33
anatarajan2 <ashokkumar_natarajan@intuit.com>
2014-10-07 15:33:31 +0530

    Adding new file compare api

--- FrameworkLibraries/Utils/FileOperations.cs
+++ FrameworkLibraries/Utils/FileOperations.cs
@@ -107,5 +107,24 @@ namespace FrameworkLibraries.Utils
                 w.Close();
             }
         }
+
+        public static bool FileEquals(string path1, string path2)
+        {
+            byte[] file1 = File.ReadAllBytes(path1);
+            byte[] file2 = File.ReadAllBytes(path2);
+            if (file1.Length == file2.Length)
+            {
+                for (int i = 0; i < file1.Length; i++)
+                {
+                    if (file1[i] != file2[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+
     }
 }
